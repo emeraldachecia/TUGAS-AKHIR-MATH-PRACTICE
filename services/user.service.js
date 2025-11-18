f9e4b501cda0c4a4a1625b6548b37ec823909616
emeraldachecia <emeraldachecia@gmail.com>
2025-11-18 21:32:01 +0700

    service 1

--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,29 @@
 const UserRepository = require("../repositories/user.repository");
 const passwordHandler = require("../utils/password-handler");
 const Connection = require("../configs/database.config");
+const { filterHandler } = require("../utils/filter-handler");
+
+class UserService {
+    async find(data, type) {
+        try {
+            let result = {};
+
+            const filters = filterHandler(data);
+            
+            switch (type) {
+				case "summary":
+					result = await UserRepository.findMany(filters);
+					break;
+				case "detail":
+					result = await UserRepository.findOne(filters);
+					break;
+				default:
+					throw new Error(`Unsupported type: ${type}`);
+			}
+
+			return result;
+		} catch (error) {
+			throw error;
+		}
+    }
+}
